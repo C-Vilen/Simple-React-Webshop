57a12a5d2eca3909c55ce0581671211ecf8c3425
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2023-04-21 13:38:18 +0200

    Done with CatCard & CatCardContainer

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './components/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import Header from './components/Header';
 import CatCardContainer from './components/CatCardContainer';
 
