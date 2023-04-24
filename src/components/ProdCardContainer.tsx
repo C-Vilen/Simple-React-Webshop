09432de6d1116e64f35f97a0bdcb2bfba3b09f55
lewinther <125894035+lewinther@users.noreply.github.com>
2023-04-24 15:04:02 +0200

    Fragment update

--- src/components/ProdCardContainer.tsx
+++ src/components/ProdCardContainer.tsx
@@ -1,26 +1,27 @@
+import { Fragment } from "react";
 import ProdCard from "./ProdCard";
 
 interface ProdCardContainerProps {
-    heading:String;
+  heading: String;
 }
 
-export default function ProdCardContainer({heading}:ProdCardContainerProps) {
-    return (
-        <>
-        <section className="py-5">
+export default function ProdCardContainer({ heading }: ProdCardContainerProps) {
+  return (
+    <Fragment>
+      <section className="py-5">
         <div className="container px-4 px-lg-5">
-            <h2 className="category-heading">{heading}</h2>
-            <div className="container row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
-                <div className="col">
-                    <ProdCard 
-                    prodImg= "assets/images/products/duck1.png" 
-                    prodName= "Viking" 
-                    prodPrice= "10"
-                    />
-                </div>
+          <h2 className="category-heading">{heading}</h2>
+          <div className="container row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
+            <div className="col">
+              <ProdCard
+                prodImg="assets/images/products/duck1.png"
+                prodName="Viking"
+                prodPrice="10"
+              />
             </div>
+          </div>
         </div>
-    </section>
-        </>
-    )
+      </section>
+    </Fragment>
+  );
 }
