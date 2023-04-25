46935ec0308ae83555acfd3dd2dfa5790b8b8dc5
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2023-04-25 11:03:20 +0200

    Removing standard styling and fixing it for the login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
 import Home from './pages/home/Home';
 import OverviewProducts from './pages/overviewProducts/OverviewProducts';
 import NoPage from './pages/noPage/NoPage';
