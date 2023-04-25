4397c055c01321c953422615a2db792247c37853
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2023-04-25 20:53:55 +0200

    NoPage styling

--- src/pages/noPage/NoPage.tsx
+++ src/pages/noPage/NoPage.tsx
@@ -3,13 +3,15 @@ import { Fragment } from 'react';
 // CSS import
 import '../../styles/index.css';
 import '../../styles/styles.css';
+import './NoPage.css'
 
 export default function NoPage() {
     return (
         <Fragment>
-            <div className="container">
-                <h1 className='text-center'>404: Try another URL</h1>
-                <img className='' src="assets/images/sad-duck.jpeg" alt=""></img>
+            <div className="container noPage">
+                <h1>404: This URL doesn't exist.</h1>
+                <h4>Please try another URL.</h4>
+                <img className='sadDuck' src="assets/images/sad-duck.jpeg" alt=""></img>
             </div>
         </Fragment>
     );
